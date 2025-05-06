2f5330647274e22295576f0e004008c7188afc19
CHartling <111919979+CHartling@users.noreply.github.com>
2025-05-06 16:10:42 -0700

    Fixed the platform issue

--- FireboyWatergirl/Assets/Scripts/PlatformerPlayerArrow.cs
+++ FireboyWatergirl/Assets/Scripts/PlatformerPlayerArrow.cs
@@ -96,15 +96,52 @@ public class PlatformerPlayerArrow : MonoBehaviour
             transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
         }
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); //
+        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); //
 
-        
+        Vector3 max = box.bounds.max;
+        Vector3 min = box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
 
+        isGrounded = false;
+        if (hit != null) {
+            isGrounded = true;
+        }
+
+        body.gravityScale = (isGrounded && Mathf.Approximately(deltaX, 0)) ? 0 : 1;
+        if (isGrounded && Input.GetKeyDown(KeyCode.UpArrow)) {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        MovingPlatform platform = null;
+        if (hit != null) {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+        if (platform != null) {
+            transform.parent = platform.transform;
+        }
+        else {
+            transform.parent = null;
+        }
+
+        anim.SetFloat("speed", Mathf.Abs(deltaX));
+
+        Vector3 pScale = Vector3.one;
+        if (platform != null) {
+            pScale = platform.transform.localScale;
+        }
+        if (!Mathf.Approximately(deltaX, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1 / pScale.y, 1);
+        }
+
+        /*
         if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
             body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             audioManager.PlaySFX(audioManager.jump);
         }
+        */
     }
 }
 
--- FireboyWatergirl/Assets/Scripts/PlatformerPlayerKey.cs
+++ FireboyWatergirl/Assets/Scripts/PlatformerPlayerKey.cs
@@ -95,15 +95,51 @@ public class PlatformerPlayerKey : MonoBehaviour
             transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
         }
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); //
+        // isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); //
+        Vector3 max = box.bounds.max;
+        Vector3 min = box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
 
-        
+        isGrounded = false;
+        if (hit != null) {
+            isGrounded = true;
+        }
 
+        body.gravityScale = (isGrounded && Mathf.Approximately(deltaX, 0)) ? 0 : 1;
+        if (isGrounded && Input.GetKeyDown(KeyCode.W)) {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        MovingPlatform platform = null;
+        if (hit != null) {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+        if (platform != null) {
+            transform.parent = platform.transform;
+        }
+        else {
+            transform.parent = null;
+        }
+
+        anim.SetFloat("speed", Mathf.Abs(deltaX));
+
+        Vector3 pScale = Vector3.one;
+        if (platform != null) {
+            pScale = platform.transform.localScale;
+        }
+        if (!Mathf.Approximately(deltaX, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1 / pScale.y, 1);
+        }
+
+        /*
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             audioManager.PlaySFX(audioManager.jump);
         }
+        */
     }
 }
 
